0c62e2ad0e48b3572755ac1c25333799f1b1adb2
adeshhg <adeshawm11@gmail.com>
2025-04-25 19:12:43 +0545

    updated

--- calculator.c
+++ calculator.c
@@ -1,18 +1,83 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <math.h>
+
+void print_menu();
+double div_func(double, double);
+int mod(int, int);
+
+int main() {
     int choice;
-    do{
-    printf("Welcome to Simple Calculator\n");
-    printf("\nChoose a option:\n");
-    printf("\n1.Addition");
-    printf("\n2.Subtraction");
-    printf("\n3.Multiplication");
-    printf("\n4.Division");
-    printf("\n5.Modulus");
-    printf("\n6.Power");
-    printf("\n7.Exit");
-    printf("\nNow, Enter your choice:");
-    scanf("%d",&choice);
-    }while (choice!=7);
+    double first, second, result;
+    while(1) {
+        print_menu();
+        scanf("%d", &choice);
+        if (choice == 7) {
+            break;
+        }
+
+        printf("\nPlease enter the first number: ");
+        scanf("%lf", &first);
+        printf("\nPlease enter the second number: ");
+        scanf("%lf", &second);
+
+        switch (choice) {
+            case 1: // Addition
+                result = first + second;
+                break;
+            case 2: // Subtraction
+                result = first - second;
+                break;
+            case 3: // Multiplication
+                result = first * second;
+                break;
+            case 4: // Division
+                result = div_func(first, second);
+                break;
+            case 5: // Modulus
+                result = mod((int)first, (int)second);
+                break;
+            case 6: // Power
+                result = pow(first, second);
+                break;
+            default:
+                printf("Invalid choice!\n");
+                continue;
+        }
+
+        printf("Result of the operation = %.2lf\n", result);
+    }
     return 0;
+}
+
+void print_menu() {
+    printf("Welcome to Simple Calculator\n");
+    printf("\nChoose an option:\n");
+    printf("\n1. Addition");
+    printf("\n2. Subtraction");
+    printf("\n3. Multiplication");
+    printf("\n4. Division");
+    printf("\n5. Modulus");
+    printf("\n6. Power");
+    printf("\n7. Exit");
+    printf("\nNow, Enter your choice: ");
+}
+
+double div_func(double a, double b) {
+    if (b == 0) {
+        fprintf(stderr, "Error! Division by zero.\n");
+        return NAN;  
+    } else {
+        return a / b;
+    }
+}
+
+int mod(int a, int b) {
+    if (b == 0) {
+        fprintf(stderr, "Error! Modulus by zero.\n");
+        return -1;  
+    } else {
+        return a % b;
+    }
+    printf("Develoved by Adesh Gurung.");
+    printf("Email:adeshawm11@gmail.com");
 }
